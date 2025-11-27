731703615c3f314db3707ed94ea1ad1e5484f43d
MrFish486 <laszlodeanpav@gmail.com>
2025-11-26 17:59:43 -0800

    Init

---
+++ Boustrophedon.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Boustrophedon {
+	public Token[] Source;
+	public File    U;
+	public char    V;
+	public String  W;
+	public int     X;
+	public float   y;
+	public boolean z;
+	public Boustrophedon (String program) {
+		this.U = new File("/dev/stdout");
+	}
+}
